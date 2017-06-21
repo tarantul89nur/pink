34a1304ed9cdc9a66e5a879dcbe687a776d88ec0
tarantul89nur <vickulbakin@mail.ru>
2017-06-21 15:04:55 +0300

    стилизовал price во всех разрешениях

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//menu--------------------------------------------------------------------------
 var menuDrop = document.querySelector(".main-nav__toggle");
 var menuHeader = document.querySelector(".main-nav");
 var menuList = document.querySelector(".main-nav__list");
@@ -18,3 +19,78 @@ menuDrop.addEventListener("click", function(evt) {
     menuHeader.classList.remove("main-nav--menu-open");
   };
 });
+
+//price-slider------------------------------------------------------------------
+var priceBtn1 = document.querySelector("#priceBtn1");
+var priceBtn2 = document.querySelector("#priceBtn2");
+var priceBtn3 = document.querySelector("#priceBtn3");
+var priceSlide1 = document.querySelector("#priceSlide1");
+var priceSlide2 = document.querySelector("#priceSlide2");
+var priceSlide3 = document.querySelector("#priceSlide3");
+
+if (window.matchMedia('(max-width: 759px)').matches)
+{
+  priceBtn1.addEventListener("click", function(evt) {
+    if (priceBtn2.classList.contains("price__btn--active")) {
+      priceBtn2.classList.remove("price__btn--active");
+      priceBtn1.classList.add("price__btn--active");
+      priceSlide1.classList.remove("js-absolute-left");
+      priceSlide1.classList.add("js-visible-column");
+      priceSlide2.classList.add("js-absolute-right");
+      priceSlide2.classList.remove("js-visible-column");
+      priceSlide3.classList.remove("js-absolute-right");
+    };
+
+    if (priceBtn3.classList.contains("price__btn--active")) {
+      priceBtn3.classList.remove("price__btn--active");
+      priceBtn1.classList.add("price__btn--active");
+      priceSlide1.classList.add("js-visible-column");
+      priceSlide2.classList.remove("js-absolute-left");
+      priceSlide2.classList.add("js-absolute-right");
+      priceSlide3.classList.remove("js-visible-column");
+    };
+  });
+
+  priceBtn2.addEventListener("click", function(evt) {
+    if (priceBtn1.classList.contains("price__btn--active")) {
+      priceBtn1.classList.remove("price__btn--active");
+      priceBtn2.classList.add("price__btn--active");
+      priceSlide1.classList.add("js-absolute-left");
+      priceSlide1.classList.remove("js-visible-column");
+      priceSlide2.classList.add("js-visible-column");
+      priceSlide2.classList.remove("js-absolute-right");
+      priceSlide3.classList.add("js-absolute-right");
+    };
+
+    if (priceBtn3.classList.contains("price__btn--active")) {
+      priceBtn3.classList.remove("price__btn--active");
+      priceBtn2.classList.add("price__btn--active");
+      priceSlide1.classList.add("js-absolute-left");
+      priceSlide2.classList.add("js-visible-column");
+      priceSlide2.classList.remove("js-absolute-left");
+      priceSlide3.classList.add("js-absolute-right");
+      priceSlide3.classList.remove("js-visible-column");
+    };
+  });
+
+  priceBtn3.addEventListener("click", function(evt) {
+    if (priceBtn1.classList.contains("price__btn--active")) {
+      priceBtn1.classList.remove("price__btn--active");
+      priceBtn3.classList.add("price__btn--active");
+      priceSlide1.classList.remove("js-visible-column");
+      priceSlide2.classList.add("js-absolute-left");
+      priceSlide2.classList.remove("js-absolute-right");
+      priceSlide3.classList.add("js-visible-column");
+    };
+
+    if (priceBtn2.classList.contains("price__btn--active")) {
+      priceBtn2.classList.remove("price__btn--active");
+      priceBtn3.classList.add("price__btn--active");
+      priceSlide1.classList.remove("js-absolute-left");
+      priceSlide2.classList.remove("js-visible-column");
+      priceSlide2.classList.add("js-absolute-left");
+      priceSlide3.classList.remove("js-absolute-right");
+      priceSlide3.classList.add("js-visible-column");
+    };
+  });
+}
