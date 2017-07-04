51b17c391232a578baf018d35e5a79292f70421c
tarantul89nur <vickulbakin@mail.ru>
2017-07-04 14:46:30 +0300

    page-photo: стилизовал upload во всех разрешениях

--- js/script.js
+++ js/script.js
@@ -96,3 +96,64 @@ if (window.matchMedia('(max-width: 759px)').matches) {
     });
   }
 }
+
+//upload__settings--------------------------------------------------------------
+var upload = document.querySelector(".upload");
+var uploadCropSvg = document.querySelector("#uploadCropSvg");
+var uploadFillSvg = document.querySelector("#uploadFillSvg");
+var uploadContrastSvg = document.querySelector("#uploadContrastSvg");
+var uploadCropRange = document.querySelector("#uploadCropRange");
+var uploadFillRange = document.querySelector("#uploadFillRange");
+var uploadContrastRange = document.querySelector("#uploadContrastRange");
+
+if (window.matchMedia('(max-width: 759px)').matches) {
+  if (upload) {
+    uploadCropSvg.addEventListener("click", function(evt) {
+      if (uploadFillSvg.classList.contains("upload__svg--active")) {
+        uploadFillSvg.classList.remove("upload__svg--active");
+        uploadFillRange.classList.add("upload__range--js-hidden");
+        uploadCropSvg.classList.add("upload__svg--active");
+        uploadCropRange.classList.remove("upload__range--js-hidden");
+      };
+
+      if (uploadContrastSvg.classList.contains("upload__svg--active")) {
+        uploadContrastSvg.classList.remove("upload__svg--active");
+        uploadContrastRange.classList.add("upload__range--js-hidden");
+        uploadCropSvg.classList.add("upload__svg--active");
+        uploadCropRange.classList.remove("upload__range--js-hidden");
+      };
+    });
+
+    uploadFillSvg.addEventListener("click", function(evt) {
+      if (uploadCropSvg.classList.contains("upload__svg--active")) {
+        uploadCropSvg.classList.remove("upload__svg--active");
+        uploadCropRange.classList.add("upload__range--js-hidden");
+        uploadFillSvg.classList.add("upload__svg--active");
+        uploadFillRange.classList.remove("upload__range--js-hidden");
+      };
+
+      if (uploadContrastSvg.classList.contains("upload__svg--active")) {
+        uploadContrastSvg.classList.remove("upload__svg--active");
+        uploadContrastRange.classList.add("upload__range--js-hidden");
+        uploadFillSvg.classList.add("upload__svg--active");
+        uploadFillRange.classList.remove("upload__range--js-hidden");
+      };
+    });
+
+    uploadContrastSvg.addEventListener("click", function(evt) {
+      if (uploadCropSvg.classList.contains("upload__svg--active")) {
+        uploadCropSvg.classList.remove("upload__svg--active");
+        uploadCropRange.classList.add("upload__range--js-hidden");
+        uploadContrastSvg.classList.add("upload__svg--active");
+        uploadContrastRange.classList.remove("upload__range--js-hidden");
+      };
+
+      if (uploadFillSvg.classList.contains("upload__svg--active")) {
+        uploadFillSvg.classList.remove("upload__svg--active");
+        uploadFillRange.classList.add("upload__range--js-hidden");
+        uploadContrastSvg.classList.add("upload__svg--active");
+        uploadContrastRange.classList.remove("upload__range--js-hidden");
+      };
+    });
+  }
+}
